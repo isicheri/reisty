e814249c2e51a35f37ceb49f6d37b357b50dd850
david <davesmithade@gmail.com>
2023-04-25 22:53:03 +0100

    card.jsx
    updates on the cards

--- src/components/cardFavourite/Cardprops.jsx
+++ src/components/cardFavourite/Cardprops.jsx
@@ -4,14 +4,14 @@ const Cardprops = (props) => {
   return (
     <>
     <div className="">
-      <div className="card bg-base-100 h-[55vh] w-[95%] border-2 border-gray-200">
-        <div className="h-[35vh]">
+      <div className="card bg-base-100 h-[315px] w-[95%] border-2 border-gray-200 hover:shadow-top">
+        <div className="h-[200px]">
           <img
             src={props.image}
             alt=""
           />
         </div>
-        <div className="card-body p-6">
+        <div className="card-body px-4 pt-4">
           <h2 className="card-title text-[20px] text-[#2B2B2B] font-medium">{props.name}</h2>
           <p>{props.location}</p>
           <div className="card-actions text-[16px] text-[#2B2B2B] font-normal">
