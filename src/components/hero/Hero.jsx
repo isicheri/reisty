70d8578ebba748a2448926cae251d3d4333e4e80
david <davesmithade@gmail.com>
2023-04-27 04:23:04 +0100

    Update Hero.jsx
    updates

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <h2>Discover the perfect table for any occasion with ease.</h2>
         </div>
 
-        <div className="w-[839px] text-[17px] flex items-center text-center font-medium leading-[22px] pr-16">
+        <div className="w-[830px] text-[17px] flex items-center text-center font-medium leading-[22px] pr-16">
             <p className='w-[839px]'>Browse through a wide selection of restaurants and effortlessly find the perfect table for any occasion, whether it's a romantic dinner for two, a birthday celebration with friends, or a business meeting with colleagues.</p>
         </div>
        </div>
