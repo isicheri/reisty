a4b85622ff4d173b90366c678ea770078121e4b9
david <davesmithade@gmail.com>
2023-06-21 12:34:16 +0100

    Ownerclaim.jsx

--- src/components/restaurantOwners/OwnersClaim.jsx
+++ src/components/restaurantOwners/OwnersClaim.jsx
@@ -4,9 +4,9 @@ import React from "react";
 const OwnersClaim = () => {
   return (
     <div className="w-[100%] mb-8 grid grid-cols-1 lg:grid-cols-2 mt-16 gap-5">
-      <div className="first px-[26px] pt-[82px] pb-4 lg:grid h-[450px] w-[350px] lg:w-[550px] lg:h-[463px] text-white lg:py-[45px] lg:pl-[30px]">
+      <div className="first px-[26px] pt-[82px] pb-4 lg:grid h-[450px] w-[345px] lg:w-[550px] lg:h-[463px] text-white lg:py-[45px] lg:pl-[30px]">
         <h2 className=" leading-[17.26px] uppercase text-[16px] font-bold">Restuarant owners</h2>
-        <h1 className=" pb-[15px] leading-[30px] w-[275px] text-[20px] lg:text-[40px] font-bold lg:leading-[50px] lg:w-[300px]">CLAIM YOUR RESTAURANT TODAY!</h1>
+        <h1 className=" pb-[15px] leading-[30px] w-[270px] text-[20px] lg:text-[40px] font-bold lg:leading-[50px] lg:w-[300px]">CLAIM YOUR RESTAURANT TODAY!</h1>
         <p className="pb-[20px] text-[14px] w-[310px] lg:w-[510px] lg:text-[17px] font-medium leading-5 lg:pt-0">Claim your restaurant on Reisty to take control of your online presence, manage reservations, menus, and reviews, and showcase your establishment to potential diners while maintaining full control."</p>
         <button className="p-1 rounded-xl bg-white text-black w-[130px] font-medium">See full list</button>
       </div>
