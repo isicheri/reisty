1567a22ba9485082ef7469f1be3c1ce98570c25f
david <davesmithade@gmail.com>
2023-07-15 01:57:15 -0700

    update

--- src/components/cardFavourite/Cardprops.jsx
+++ src/components/cardFavourite/Cardprops.jsx
@@ -8,8 +8,8 @@ import Imgreen from "../../assets/imggreenchairs.svg";
 import { HiHeart } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import RatingStar from "../rating/RatingStar";
-import ForwardArrow from "../../assets/forwardarrow.svg"
-import BackwardArrow from "../../assets/backwardarrow.svg"
+import ForwardArrow from "../../assets/forwardarrow.svg";
+import BackwardArrow from "../../assets/backwardarrow.svg";
 // import BackwardArrow from "../../assets/backwardarrow.png"
 
 const Cardprops = (props) => {
@@ -33,73 +33,83 @@ const Cardprops = (props) => {
   return (
     <>
       <div className=" min-w-max lg:min-w-fit" style={{ marginRight: "1rem" }}>
-        <Link to="/">
-          <div className="hovshade card relative bg-base-100 h-[330px] lg:max-h-min border-2 border-gray-200">
-            <div
-              style={{ backgroundImage: "url(" + allBgs[curSlide] + ")" }}
-              className="picPlace relative h-[205px] lg:h-[215px] bg-red-50 lg:w-full w-[250px] "
-            >
-              <div className="heartFavourite w-full flex justify-end p-3">
-                <HiHeart
-                  size={30}
-                  color="white"
-                  onClick={(e) => {
-                    e.target.style.color == "white"
-                      ? (e.target.style.color = "#f43f5a")
-                      : (e.target.style.color = "white");
-                  }}
-                  className="p-1 rounded-full bg-[#00000020] active:scale-90 duration-100 cursor-pointer"
-                />
-              </div>
-
-              <div className="arrows flex justify-between w-full px-4 absolute ">
-                <div
-                  style={
-                    curSlide > 0
-                      ? { visibility: "visible" }
-                      : { visibility: "hidden" }
-                  }
-                  className="arrow w-5 h-5 rounded-full"
-                  onClick={() => {
-                    setSlide((cur) => cur - 1);
-                  }}
-                >
-                  <img src={BackwardArrow} alt="" className="text-[50px]"/>
-                </div>
-
-                <div
-                  style={
-                    curSlide < allBgs.length - 1
-                      ? { visibility: "visible" }
-                      : { visibility: "hidden" }
-                  }
-                  className="arrow w-5 h-5 rounded-full"
-                  onClick={() => {
-                    setSlide((cur) => cur + 1);
-                  }}
-                >
-                 <img src={ForwardArrow} alt="" />
-
-                </div>
-              </div>
-
-              <div className=" stepper opacity-0 w-full flex gap-1 justify-center items-center text-yellow-500 absolute bottom-2 left-1/2 -translate-x-1/2 h-2">
-                <div style={curSlide === 0? {opacity: 1} : {opacity: '45%'}} className="w-2 h-2 rounded-full bg-white"></div>
-                <div style={curSlide === 1? {opacity: 1} : {opacity: '45%'}} className="w-2 h-2 rounded-full bg-white"></div>
-                <div style={curSlide === 2? {opacity: 1} : {opacity: '45%'}} className="w-2 h-2 rounded-full bg-white"></div>
-                <div style={curSlide === 3? {opacity: 1} : {opacity: '45%'}} className="w-2 h-2 rounded-full bg-white"></div>
-              </div>
-
+        <div className="hovshade card relative bg-base-100 h-[330px] lg:max-h-min border-2 border-gray-200">
+          <div
+            style={{ backgroundImage: "url(" + allBgs[curSlide] + ")" }}
+            className="picPlace relative h-[205px] lg:h-[215px] bg-red-50 lg:w-full w-[250px] "
+          >
+            <div className="heartFavourite w-full flex justify-end p-3">
+              <HiHeart
+                size={30}
+                color="white"
+                onClick={(e) => {
+                  e.target.style.color == "white"
+                    ? (e.target.style.color = "#f43f5a")
+                    : (e.target.style.color = "white");
+                }}
+                className="p-1 rounded-full bg-[#00000020] active:scale-90 duration-100 cursor-pointer"
+              />
             </div>
-            <div className="card-body px-4 pt-8 lg:pt-2">
-              <h2 className="card-title text-[20px] text-[#2B2B2B] font-semibold">
-                {props.name}
-              </h2>
-              <p className="pb-2 font-light">{props.location}</p>
-              <RatingStar />
+
+            <div className="arrows flex justify-between w-full px-4 absolute ">
+              <div
+                style={
+                  curSlide > 0
+                    ? { visibility: "visible" }
+                    : { visibility: "hidden" }
+                }
+                className="arrow w-5 h-5 rounded-full"
+                onClick={() => {
+                  setSlide((cur) => cur - 1);
+                }}
+              >
+                <img src={BackwardArrow} alt="" className="text-[50px]" />
+              </div>
+
+              <div
+                style={
+                  curSlide < allBgs.length - 1
+                    ? { visibility: "visible" }
+                    : { visibility: "hidden" }
+                }
+                className="arrow w-5 h-5 rounded-full"
+                onClick={() => {
+                  setSlide((cur) => cur + 1);
+                }}
+              >
+                <img src={ForwardArrow} alt="" />
+              </div>
+            </div>
+
+            <div className=" stepper opacity-0 w-full flex gap-1 justify-center items-center text-yellow-500 absolute bottom-2 left-1/2 -translate-x-1/2 h-2">
+              <div
+                style={curSlide === 0 ? { opacity: 1 } : { opacity: "45%" }}
+                className="w-2 h-2 rounded-full bg-white"
+              ></div>
+              <div
+                style={curSlide === 1 ? { opacity: 1 } : { opacity: "45%" }}
+                className="w-2 h-2 rounded-full bg-white"
+              ></div>
+              <div
+                style={curSlide === 2 ? { opacity: 1 } : { opacity: "45%" }}
+                className="w-2 h-2 rounded-full bg-white"
+              ></div>
+              <div
+                style={curSlide === 3 ? { opacity: 1 } : { opacity: "45%" }}
+                className="w-2 h-2 rounded-full bg-white"
+              ></div>
             </div>
           </div>
-        </Link>
+          <Link to="/restaurantProfile">
+          <div className="card-body px-4 pt-8 lg:pt-2">
+            <h2 className="card-title text-[20px] text-[#2B2B2B] font-semibold">
+              {props.name}
+            </h2>
+            <p className="pb-2 font-light">{props.location}</p>
+            <RatingStar />
+          </div>
+          </Link>
+        </div>
       </div>
     </>
   );
